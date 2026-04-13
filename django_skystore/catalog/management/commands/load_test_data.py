eb5e5337f29d0eda048ea1e6f34d4ded4da87d62
Roman <romar.m.git@gmail.com>
2026-04-13 11:43:34 +0300

    21.23

---
+++ django_skystore/catalog/management/commands/load_test_data.py
@@ -0,0 +1,18 @@
+import os
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Category, Product
+
+class Command(BaseCommand):
+    help = 'Загружает тестовые данные из фикстур, очищая существующие'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Очистка существующих данных...')
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        self.stdout.write('Загрузка фикстур...')
+        call_command('loaddata', 'catalog/fixtures/categories.json')
+        call_command('loaddata', 'catalog/fixtures/products.json')
+
+        self.stdout.write(self.style.SUCCESS('Данные успешно загружены'))
