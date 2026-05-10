72db3f868ba24760b92a16a59a7db42fd2d6c30d
Romar <@>
2026-05-10 22:04:21 +0300

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import BlogPost
@@ -8,7 +9,6 @@ class BlogPostListView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        # Только опубликованные статьи
         return BlogPost.objects.filter(is_published=True)
 
 class BlogPostDetailView(DetailView):
@@ -18,31 +18,27 @@ class BlogPostDetailView(DetailView):
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
-        # Увеличиваем счётчик просмотров
         obj.views_count += 1
         obj.save()
-        # (опционально) проверка на 100 просмотров и отправка письма
         if obj.views_count == 100:
-            # Здесь можно отправить email
             print(f"Поздравляем! Статья '{obj.title}' набрала 100 просмотров!")
         return obj
 
-class BlogPostCreateView(CreateView):
+class BlogPostCreateView(LoginRequiredMixin, CreateView):
     model = BlogPost
     template_name = 'blog/blogpost_form.html'
     fields = ['title', 'content', 'preview', 'is_published']
     success_url = reverse_lazy('blog:list')
 
-class BlogPostUpdateView(UpdateView):
+class BlogPostUpdateView(LoginRequiredMixin, UpdateView):
     model = BlogPost
     template_name = 'blog/blogpost_form.html'
     fields = ['title', 'content', 'preview', 'is_published']
 
     def get_success_url(self):
-        # Перенаправление на страницу просмотра статьи после редактирования
         return reverse_lazy('blog:detail', kwargs={'pk': self.object.pk})
 
-class BlogPostDeleteView(DeleteView):
+class BlogPostDeleteView(LoginRequiredMixin, DeleteView):
     model = BlogPost
     template_name = 'blog/blogpost_confirm_delete.html'
     success_url = reverse_lazy('blog:list')
--- users/views.py
+++ users/views.py
@@ -4,15 +4,20 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
 from .forms import UserRegisterForm, UserLoginForm
 
+from django.contrib.auth import login
+
 class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
     def form_valid(self, form):
-        response = super().form_valid(form)
+        # Сохраняем пользователя, но пока без коммита в БД
+        user = form.save(commit=False)
+        # Хешируем пароль
+        user.set_password(form.cleaned_data['password1'])
+        user.save()
         # Отправляем приветственное письмо
-        user = self.object
         send_mail(
             'Добро пожаловать в SkyStore!',
             f'Здравствуйте, {user.email}!\n\nСпасибо за регистрацию.',
@@ -20,7 +25,9 @@ class RegisterView(CreateView):
             [user.email],
             fail_silently=False,
         )
-        return response
+        # Автоматически логиним после регистрации (опционально, можно убрать)
+        # login(self.request, user)
+        return redirect(self.success_url)
 
 class UserLoginView(LoginView):
     form_class = UserLoginForm
