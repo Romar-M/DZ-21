f290537787aa7a5bbcc8c90fe1ebed7ab2938114
Roman <romar.m.git@gmail.com>
2026-05-25 10:43:24 +0300

    29

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,17 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'TIMEOUT': 300,  # таймаут кеша по умолчанию (5 минут)
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
